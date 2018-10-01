948361e033fc10243977037d7a22cec594ec2385
pfjensen <slush34@att.net>
2018-10-01 09:47:38 -0500

    mlab fix

--- server.js
+++ server.js
@@ -11,7 +11,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/newsCommentor", { useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost/newsCommentor", { useNewUrlParser: true });
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 
 app.get("/scrape", function (req, res) {
 
